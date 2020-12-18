c8d301ecae949168c8501a76e81f583ff9fcc2ce
ghsayan <sayanghosh004@gmail.com>
2020-12-18 22:18:08 +0530

    Syllabus cards now fetches data from json file

--- lib/syllabusPage.dart
+++ lib/syllabusPage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 import 'home_page.dart';
 import 'package:tree_view/tree_view.dart';
+import 'dart:convert';
+
 
 class SyllabusPage extends StatefulWidget {
   @override
@@ -9,55 +11,146 @@ class SyllabusPage extends StatefulWidget {
 }
 
 class _SyllabusPageState extends State<SyllabusPage> {
-  String jsonSyllabus;
+  Map<String, dynamic> jsonSyllabus;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await loadJson();
+    });
+    loadJson();
+  }
+
+  loadJson() async {
+    String data =
+        await DefaultAssetBundle.of(context).loadString("assets/syllabus.json");
+    jsonSyllabus = jsonDecode(data) as Map<String, dynamic>;
+  }
+
+  List<Parent> getStream() {
+    List<Parent> childs = [];
+    for (var i = 0; i < 3; i++) {
+      childs.add(
+        Parent(
+          parent: SyllabusCard(
+            name: jsonSyllabus['syllabus'][i]['stream'],
+            level: 0,
+          ),
+          childList: ChildList(
+            children: <Widget>[
+              for (var j = 0; j < 3; j++)
+                Parent(
+                  parent: SyllabusCard(
+                    name: "Semester" + ((j + 1).toString()),
+                    level: 1,
+                  ),
+                  childList: ChildList(
+                    children: <Widget>[
+                      for (var k = 0; k < 3; k++)
+                        SyllabusCard(
+                          name: jsonSyllabus['syllabus'][k]['semester'][j][k],
+                          level: 2,
+                        ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ),
+      );
+    }
+    return childs;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff16697a),
       body: TreeView(
-        parentList: [
-          Parent(
-            parent: SyllabusCard(
-              name: "Computer Science",
-              level: 0,
-            ),
-            childList: ChildList(
-              children: [
-                Parent(
-                  parent: SyllabusCard(
-                    name: "Semester 1",
-                    level: 1,
-                  ),
-                  childList: ChildList(
-                    children: <Widget>[
-                      SyllabusCard(
-                        name: "Data Structure and Analysis",
-                        level: 2,
-                      ),
-                      SyllabusCard(
-                        name: "Computer Organization",
-                        level: 2,
-                      ),
-                    ],
-                  ),
-                ),
-                SyllabusCard(
-                  name: "Semester 2",
-                  level: 1,
-                ),
-                SyllabusCard(
-                  name: "Semester 3",
-                  level: 1,
-                ),
-                SyllabusCard(
-                  name: "Semester 4",
-                  level: 1,
-                ),
-              ],
-            ),
-          )
-        ],
+        parentList: getStream(),
+
+        //     [
+        //   Parent(
+        //     parent: SyllabusCard(
+        //       name: "Computer Science",
+        //       level: 0,
+        //     ),
+        //     childList: ChildList(
+        //       children: [
+        //         Parent(
+        //           parent: SyllabusCard(
+        //             name: "Semester 1",
+        //             level: 1,
+        //           ),
+        //           childList: ChildList(
+        //             children: <Widget>[
+        //               SyllabusCard(
+        //                 name: "Data Structure and Analysis",
+        //                 level: 2,
+        //               ),
+        //               SyllabusCard(
+        //                 name: "Computer Organization",
+        //                 level: 2,
+        //               ),
+        //             ],
+        //           ),
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 2",
+        //           level: 1,
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 3",
+        //           level: 1,
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 4",
+        //           level: 1,
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        //   Parent(
+        //     parent: SyllabusCard(
+        //       name: "Computer Science",
+        //       level: 0,
+        //     ),
+        //     childList: ChildList(
+        //       children: [
+        //         Parent(
+        //           parent: SyllabusCard(
+        //             name: "Semester 1",
+        //             level: 1,
+        //           ),
+        //           childList: ChildList(
+        //             children: <Widget>[
+        //               SyllabusCard(
+        //                 name: "Data Structure and Analysis",
+        //                 level: 2,
+        //               ),
+        //               SyllabusCard(
+        //                 name: "Computer Organization",
+        //                 level: 2,
+        //               ),
+        //             ],
+        //           ),
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 2",
+        //           level: 1,
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 3",
+        //           level: 1,
+        //         ),
+        //         SyllabusCard(
+        //           name: "Semester 4",
+        //           level: 1,
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ],
       ),
     );
   }
@@ -78,26 +171,24 @@ class SyllabusCard extends StatelessWidget {
       padding: EdgeInsets.only(
         left: (level * 40) + 8.0,
         right: 8.0,
-        top: 8.0,
+        top: level == 0 ? 20.0 : 0.0,
         bottom: 0.0,
       ),
       child: Card(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18.0),
+          borderRadius: BorderRadius.circular(6.0),
         ),
-        elevation: 6.0,
+        elevation: 9.0,
         shadowColor: Colors.white,
-        color: Colors.white,
+        color: Colors.white.withOpacity(1 - (0.2 * level)),
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Center(
-            child: Text(
-              name,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontFamily: 'RobotoMono',
-                fontWeight: FontWeight.bold,
-              ),
+          child: Text(
+            name,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontFamily: 'RobotoMono',
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
